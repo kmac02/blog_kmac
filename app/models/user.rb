2fbcec5f129b7481043b423eb6d00e0b88affed5
Kristin MacDonough <email": null>
2017-10-29 18:03:43 -0500

    layour edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,28 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  # Below included for lightning talk
+  # include BCrypt
+
+  # def password
+  #   @password ||=Password.new(hashed_password)
+  # end
+
+  # def password=(new_password)
+  #   @user_password = new_password
+  #   @password = Password.create(new_password)
+  #   self.hashed_password = @password
+  # end
+
+  # def check_password
+  #   if @user_password.empty?
+  #     errors.add :password, "can't be blank"
+  #     return
+  #   end
+  # end
+
+  # def authenticate(submitted_password)
+  #   self.password == submitted_password
+  # end
+
 end
