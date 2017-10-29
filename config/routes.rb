042182858c7561e7a9d0ef62c5bb16c12992c774
Kristin MacDonough <email": null>
2017-10-29 14:44:34 -0500

    Add user create and login routes with has_secure_password

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,15 @@ Rails.application.routes.draw do
   root "posts#index"
 
   get '/about', to: "pages#about"
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 end
